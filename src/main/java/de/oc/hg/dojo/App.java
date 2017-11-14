df49735c156f8214b2afc2720df2e0f3ee8a3882
Stefan Billeb <stefan.billeb@opitz-consulting.com>
2017-11-14 19:17:04 +0100

    Feature complete for tree solution

--- src/main/java/de/oc/hg/dojo/App.java
+++ src/main/java/de/oc/hg/dojo/App.java
@@ -24,6 +24,7 @@ public class App
         numberLine.add(br.readLine());
 
         for(int nums=0; nums < 9; nums++) {
+            System.out.println();
             StringBuilder number = new StringBuilder();
             for (int line = 0; line < 3; line++) {
                 for (int pos = nums*3; pos < (nums*3)+3; pos++) {
@@ -32,7 +33,7 @@ public class App
             }
 
         String num = number.toString();
-        System.out.print(num);
+        System.out.print(num + " = ");
 
         ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
 
@@ -63,6 +64,7 @@ public class App
                         numbers.remove(new Integer(8));
                         numbers.remove(new Integer(9));
                     } else if(PIPE == num.charAt(pos)) {
+                        numbers.remove(new Integer(1));
                         numbers.remove(new Integer(2));
                         numbers.remove(new Integer(3));
                         numbers.remove(new Integer(7));
@@ -70,33 +72,44 @@ public class App
                     break;
                 case 5:
                     if (EMPTY == num.charAt(pos)) {
+                        numbers.remove(new Integer(2));
+                        numbers.remove(new Integer(3));
                         numbers.remove(new Integer(5));
                         numbers.remove(new Integer(6));
                         numbers.remove(new Integer(8));
                         numbers.remove(new Integer(9));
                     } else if(UNDERSCORE == num.charAt(pos)) {
+                        numbers.remove(new Integer(0));
                         numbers.remove(new Integer(7));
                     }
                     break;
                 case 6:
                     if(PIPE == num.charAt(pos)) {
-                        numbers.remove(new Integer(2));
-                        numbers.remove(new Integer(3));
+                        numbers.remove(new Integer(5));
+                        numbers.remove(new Integer(6));
+                    } else if(EMPTY == num.charAt(pos)) {
+                        numbers.remove(new Integer(8));
+                        numbers.remove(new Integer(9));
                     }
                     break;
                 case 7:
                     if(PIPE == num.charAt(pos)) {
+                        numbers.remove(new Integer(5));
                         numbers.remove(new Integer(3));
+                        numbers.remove(new Integer(9));
                     } else if(EMPTY == num.charAt(pos)) {
                         numbers.remove(new Integer(2));
+                        numbers.remove(new Integer(6));
+                        numbers.remove(new Integer(8));
                     }
                     break;
             }
 
             if(numbers.size() == 1) {
-                System.out.println(" = " + numbers.get(0));
+                System.out.print(numbers.get(0));
                 break;
             }
+
         }
 
     }
